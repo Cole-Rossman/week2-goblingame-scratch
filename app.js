b36e4be256186c419c86ae4fc3710d7cf1bd64b4
Cole Rossman <colerossman99@gmail.com>
2022-02-02 17:30:34 -0800

    in app.js functions imported, dom elements grabbed, let state complete

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // import functions and grab DOM elements
+import { renderOrc } from './render-utils.js';
+
+const defeatedNumber = document.getElementById('defeated-number');
+const heroHP = document.getElementById('hero-hp');
+const heroImage = document.getElementById('hero-image');
+const orcForm = document.getElementById('form');
+const orcInfo = document.getElementById('orc');
 
 // let state
+let defeatedOrcCount = 0;
+let playerHP = 1;
+let orcs = [
+    { id: 1, name: 'Sheldon', hp: 1 },
+    { id: 2, name: 'Cletus', hp: 1 },
+    { id: 3, name: 'Arthur', hp: 1 },
+];
+let currentId = 4;
 
 // set event listeners 
   // get user input
